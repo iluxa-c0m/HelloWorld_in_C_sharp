fcd79f992b85619c4b048be18937d331d54a7d43
ilya.komolkin <ilya.komolkin@jetbrains.com>
2016-05-12 16:06:42 +0300

    1

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -15,6 +15,7 @@ namespace HelloWorld
             Console.WriteLine("and again!!!!");
             Console.WriteLine("Hi again!!!!");
             Console.WriteLine("Hi again!!!!");
+            Console.WriteLine("!!!!");
         }
     }
 }
